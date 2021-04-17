239996dd080cef48588ce856ddaadb54bc287a72
chief bazinga <71347819+the-real-sumsome@users.noreply.github.com>
2021-04-17 13:54:09 -0700

    polish translation by evodesign

---
+++ pl.php
@@ -0,0 +1,275 @@
+<?php
+    // [TODO]
+    $cLang = array(
+        "languageD" => "Polish",
+
+        // [categories]
+        "recentC" => "Najnowsze filmy na FulpTube - Globalnie",
+        "musicC" => "Muzyka",
+        "reccomendedC" => "Rekomendowane",
+        "videoC" => "Gry",
+        // [video] 
+        "byUsr" => "przez",
+        "agoLeft" => "parę",
+        "agoRight" => "<agoLeft> <date> <agoRight> ", // <agoLeft> <date> <agoRight> ex: 3 seconds ago
+        "justNow" => "teraz",
+        "ago" => "temu",
+        "country" => "Kraj",
+        "eViews" => "wyświetleń",
+        "worldwide" => "Globalny",
+        "gotIt" => "Rozumiem!",
+        "welcomeToFulpTubeHomepage" => "Witaj na stronie głównej FulpTube",
+        // [sidebar]
+        "channelsForYou" => "Kanały dla ciebie",
+        "dashboardText" => "Filmy z twojej listy subskrybowanych kanałów i personalizowanych rekomendacji filmów bedą wyświetlane tutaj.
+FulpTube sortuje te filmy na podstawie tego, co prawdopodobnie będziesz chciał/a obejrzeć.<br><br>
+        Chcesz zobaczyć wszystkie nowości z twoich subskrybcji? <br>
+        Sprawdź <a href=\"#\">Moje Subskrybcje</a>.",
+        "subscriptions" => "Subskrybcje",
+        "subscriptionManage" => "Zarządzaj Subskrybcjami",
+        "browseChannels" => "Przeglądaj Kanały",
+        "newestVideos" => "Najnowsze Filmy",
+        "signInNow" => "Zaloguj się już teraz aby zobaczyć twoje filmy, kanały i rekomendacje!",
+        // [header]
+        "uploadVid" => "Prześlij",
+        "browse" => "Przeglądaj",
+        "vChannel" => "Kanał",
+        "feed" => "Nowości",
+        "vUploaded" => "Przesłane",
+        "vCancel" => "Anuluj",
+        "vDuration" => "Długość",
+        "annoHowtoTitle" => "Instrukcja Użytku:",
+        "videoResponseStart" => "<a href=\"/videoresponse?v=",
+        "videoResponseEnd" => "\">Stwórz Odpowiedź Wideo</a> or ",
+        "vViews" => "wyświetlenia",
+        "vLikes" => "Łapki w górę",
+        "annoCreator" => "Kreator Adnotacji autorstwa Crazy Frog",
+        "vDislikes" => "Łapki w dół",
+        "toCreateAnnotations" => "Aby dodać adnotacje do filmu, musisz wybrać plik.",
+        "vStatus" => "Status",
+        "toggleCommenting" => "Wyłącz/Włącz Komentarze",
+        "vComments" => "Komentarze",
+        "annoHowto" => 'Ustaw pozycje, kolor, rozmiar i czas trwania dla twoich adnotacji.<br><br>
+        Kliknij na przycisk "Dodaj do wyjściowego filmu" aby zapisać twoje zmiany.<br><br>
+        Jeżeli nie lubisz twoich nowych zmian, kliknij na przycisk "Cofnij".<br><br>
+        Aby rozpocząć od nowa, kliknij na przycisk "Wyczyść wyjściowy film".<br><br>
+        Jeżeli jesteś zadowolony z twoich adnotacji, kliknij na "Skopiuj do schowka" aby skopiować twoje skończone adnotacje do filmu.',
+        "vURL" => "URL Filmu",
+        "actualUploadVideo" => "Prześlij Film",
+        "delete" => "Usuń",
+        "thumbnail" => "Miniaturka",
+        "upload" => "Prześlij",
+        "signIn" => "Zaloguj się",
+        // [time]
+        "year" => "rok",
+        "month" => "miesiąc",
+        "week" => "tydzień",
+        "day" => "dzien",
+        "hour" => "godzin",
+        "minute" => "minut",
+        "second" => "sekund",
+
+        "pluralTimeRight" => "s",
+        // [watch page]
+        "subscribe" => "Subskrybuj",
+        "language" => "Język",
+        "copyright" => "Prawa autorskie",
+        "about" => "O nas",
+        "shareVid" => "Udostępnij",
+        "addTo" => "Dodaj do",
+        "tags" => "Tagi",
+        "thumbnail" => "Miniaturka",
+        "charremaining" => "wolnych znaków.",
+        "rectag" => "Rekomendowane tagi",
+        "vidStatistics" => "Statystyki",
+        "showMore" => "Pokaż więcej",
+        "prePost" => "unused, do not translate",
+        "post" => "Postuj",
+        "cPost" => "Postuj",
+        "showLess" => "Pokaż mniej",
+        "reply" => "Odpowiedz",
+        "shareyourthoughts" => "Podziel się swoją opinią",
+        "allComments" => "Wszystkie komentarze",
+        "publishedAt2" => "Opublikowane",
+        "topComments" => "Najlepsze komentarze",
+        "loadMore" => "Pokaż więcej",
+        // [profile]
+        "discussion" => "Dyskusja",
+        "recentUploads" => "Ostatnio dodane",
+        "playlists" => "Playlisty",
+        "profilePic" => "Zdjęcie Profilowe",
+        "uploadBanner" => "Prześlij Baner",
+        "uploadImage" => "Prześlij Zdjęcie",
+        "joined" => "Dołączył/a",
+        "messageProfile" => "Napisz wiadomość",
+        "uploadedAVideo" => "opublikował film.",
+        "subscribers" => "subskrybentów",
+        "userJoined" => "Dołączył/a %d",
+        "featuredChannel" => "Polecane Kanały",
+        "popularChannels" => "Popularne Kanały",
+        "theseRecommendations" => "Te rekomendacje zostały automatycznie wygenerowane przez FulpTube.",
+        // [inbox]
+        "newComment" => "Nowy Komentarz",
+        "newSubscriber" => "Nowy Subskrybent",
+        "inbox" => "Skrzynka Odbiorcza",
+        "markAll" => "Oznacz wszystkie jako przeczytane",
+        "compose" => "Utwórż",
+        "from" => "Od:",
+        "sent" => "Wysłane",
+        "sendMessage" => "Wyślij Wiadomość",
+        "date" => "Data",
+        "to" => "Do:",
+        "subject" => "Temat:",
+        "messageInbox" => "Wiadomość:",
+        // [upload]
+        "uTitle" => "Tytuł",
+        "category" => "Kategoria",
+        "privacySetting" => "Ustawienia widoczności",
+        "description" => "Opis",
+        // [sign in]
+        "signInBenifits" => "Zaloguj się teraz, aby używać FulpTube do:",
+        "s1a" => "Bycia na bieżąco z twoimi ulubionymi kanałami.",
+        "s1b" => "Zapisuj filmy do obejrzenia później, oglądaj rekomendacje przygotowane specjalnie dla Ciebie lub subskrybuj, aby otrzymywać aktualizacje z ulubionych kanałów.",
+        "s2a" => "Oglądania gdziekolwiek, kiedykolwiek.", 
+        "s2b" => "Zabierz swoje ulubionych materiały gdziekolwiek jedziesz lub idziesz — oglądaj na swoim telefonie, tablecie, lub smart TV.",
+        "s3a" => "Dzielenia się ze swoimi znajomymi.",
+        "s3b" => "Oglądaj filmy twoich znajomych z innych mediów społecznościowych — wszystko w jednym miejscu.", 
+        // [misc]
+        //"" => "",
+        "username2" => "Nazwa użytkownika",
+        "password2" => "Hasło",
+        "email2" => "E-Mail",
+        "confirmpw2" => "Potwierdź hasło",
+        "register" => "Zarejestruj się",
+        "signUp" => "Zarejestruj się",
+        "orRegister" => "...albo się <a href='/newsignup'>zarejestruj</a>",
+        "unsubscribe" => "Odsubskrybuj",
+        "addChannels" => "Dodaj kanały",
+        "subscribed" => "Subskrybujesz",
+        "xVideos" => "filmy",
+        "uVideos" => "Filmy",
+        "transcribe" => "Transkrypcja",
+        "statistics" => "Statystyki",
+        "report" => "Zgłoś",
+        "popularChannels" => "Popularne kanały na FulpTube",
+        "featuredChannels" => "Polecane kanały",
+        "dateAddedoldNew" => "Data dodania (najstarsze - najnowsze)",
+        "dateAddedNewOld" => "Data dodania (najnowsze - najstarsze)",
+        "Uplaylist" => "Playlisty",
+        "mostPopular" => "Najbardziej Popularne",
+        "privacy" => "Prywatność",
+        "playlist" => "playlista",
+        "termsAndCond" => "Warunki korzystania",
+        "sendFeedback" => "Prześlij opinię",
+        "recentUploads" => "Ostatnio Opublikowane",
+        "uploads" => "Prześlij",
+        "searchSubscriptions" => "Szukaj w Subskrypcjach",
+        "comments" => "komentarz(e)",
+        "inboxComments" => "Komentarze",
+        "contactNotifications" => "Powiadomienia Kontaktów",
+        "message" => "Wiadomość",
+        "time" => "Czas:",
+        "joindate" => "Dołączył/a w ",
+        "myChannel" => "Mój kanał",
+        "videoManager" => "Menedżer filmów",
+        "fulptubeSettings" => "Ustawienia",
+        "userSettings" => "Ustawienia Użytkownika",
+        "signOut" => "Wyloguj się",
+        "foogleAccount" => "Konto Foogle",
+        "thisisyou" => "To jesteś Ty.",
+        "overview" => "Przegląd",
+        "name" => "Nazwa",
+        "password" => "Hasło",
+        "changePW" => "Zmień hasło",
+        "cemail" => "Zostanie do ciebie wysłany email z potwierdzeniem resetu hasła.",
+        "misc" => "Inne",
+        "accCreated" => "Data stworzenia konta:",
+        "loginDate" => "Ostatnie logowanie:",
+        "customization" => "Personalizacja",
+        "personalMessages" => "Prywatne Wiadomości",
+        "sharedWithYou" => "Udostępnione tobie",
+        "banner" => "Banner",
+        "resWarn" => "Notka: Rekomendowana rozdzielczość banneru to 821x176. Każda mniejsza lub więszka będzie rozciągana aby poprawnie wypełnila miejsce.",
+        "yourPhoto" => "Twoje zdjęcie",
+        "socialLinks" => "Linki do mediów społecznościowych",
+        "links" => "Linki",
+        "customURL" => "Własny adres URL",
+        "leaveBlank" => "Zostaw puste pola jeżeli nie chcesz zmieniać tych ustawień.",
+        "featureAndDescribe" => "Poleć i opisz",
+        "describeAndfeature" => "Opisz i poleć",
+        "sent" => "Wysłane",
+        "webVTT" => "Notka: tylko WebVTT jest wspierany.",
+        "edit" => "Edytuj",
+        "maxRes" => "Notka: Maksymalny rozmiar to 1080p.",
+        "bio" => "O mnie",
+        "captionsNotWorking" => "Napisy chwilowo mogą nie działać poprawnie.",
+        "anno" => "Adnotacje",
+        "basicInfo" => "Podstawowe Informacje",
+        "videoManager" => "Menedżer filmów",
+        "displayName" => "Wyświetlana Nazwa",
+        "addMoreVideos" => "Dodaj więcej filmów",
+        "featuredChannels" => "Polecane kanały",
+        "exampleFeaturedChannels" => "Oddziel za pomocą przecinków. Nie dodawaj spacji po lub przed nimi, np. chief bazinga,Sniped,Joel98",
+        "featuredVideo" => "Polecany film",
+        "warningFeaturedVideo" => "Nie może posiadać on spacji po lub przed linkiem. Na przykład: MTYwNTQ4ODc4Mja=24 jest prawidłowy. NIE DODAWAJ PRZED NIM LINKU FULPTUBE.ROCKS, ZOSTAW TYLKO ID.",
+        "toggleFlash" => "Włącz/Wyłącz Flasha",
+        "aboutResults" => "ok. %s wyników",
+        "searchSubscriptions" => "Szukaj w Subskrypcjach",
+        "watchLater" => "Do obejrzenia",
+        "donotreupload" => "                            <b style=\"font-size: 18px;\">Proszę, nie przesyłaj filmów które nie należą do ciebie.</b><br>
+        <br>
+        Miniaturki są automatycznie generowane.<br>
+        Wszystkie filmy są automatycznie konwertowane do 720p@1000kbps H264/AAC za pomocą FFMPEG, co pozwala na przesyłanie większej ilości formatów.<br>
+        Filmy muszą być mniejsze od 100MB. (to przez Cloudflare)<br>
+        Nie wrzucaj filmów objętych prawami autorskimi które obejmuje DMCA.<br>
+        <b>Filmy są manualnie sprawdzane</b>",
+        "watchHistory" => "Historia",
+        "whatToWatch" => "Co obejrzeć",
+        "mySubscriptions" => "Moje Subskrybcje",
+        "analytics" => "Statystyki",
+        "dashboard" => "Panel",
+        "adobeStatus" => "Adobe Flash no est? ",
+        "active" => "aktywny",
+        "guide" => 'Przewodnik',
+        "deactive" => "nie aktywny",
+
+        "fulptubeTOS" => "Warunki korzystania FulpTube",
+        "fulptubeTOSSub" => "                                                <b>Ogólne zasady</b><br>
+        Po prostu nie bądź ogromnym idiotą. To strona zrobiona dla zabawy.<br>
+        To jest strona poświęcona odtworzeniu YouTube'a z 2013.<br>
+        Odtwarzacz wideo oraz skrypty JS nie są naszego autorstwa, należą one do YouTube.<br>
+        FulpTube nie jest w żaden sposób związany z Google LLC.<br>
+        Wszelkie treści naruszające prawo i przepisy federalne Stanów Zjednoczonych zostaną usunięte, a powiązane z nimi konta zablokowane.<br>
+        Wszystkie próby ominięcia bana są absolutnie NIE TOLEROWANE i tego typu próby zakończą sie banem na FulpTube.<br>
+        Zakaz NSFW, bigoterii i homofobii. Po prostu kieruj się zdrowym rozsądkiem. Z góry dziękuję.<br><br>
+        <b>Zgłaszanie błędów i filmów</b><br>
+        Jeżeli widzisz film który narusza nasze warunki korzystania, zgłoś go natychmiast na naszym serwerze Discord który znajduje się na samym dole strony.<br>
+        Znalazłeś błąd? zgłoś go na kanale #bug-reports na naszym serwerze Discord.<br><br>
+        <b>Kontakt</b><br>
+        Email: spacemygithub@gmail.com<br>
+        Oficjalny serwer Discord: na dole strony<br>
+        Discord: bhief cazinga#5644",
+
+        "ftPrivacy" => "Polityka Prywatności FulpTube",
+        "ftPrivacySubtext" => "                        <b>Prywatność</b><br>
+        NIE przechowujemy adresów IP. Wszystko, co jest przechowywane, to Twoje hasło, adres e-mail i nazwa użytkownika.<br>
+        Inne przechowywane rzeczy to Twoje filmy, komentarze itp. Możesz poprosić o plik z twoimi wszystkimi danymi.<br>
+        Nie sprzedajemy danych.<br>
+        Masz obawy dotyczące prywatności? Nasz Discord znajduje się powyżej.<br>",
+        
+        "ftCopyright" => "Prawa autorskie na FulpTube",
+        "ftCopyrightSubtext" => "                                                <b>Prawa autorskie</b><br>
+        Upewnij się, że nie zarabiasz na muzyce odtwarzanej w tle filmu.<br>
+        Możesz przesyłać nieprzetworzone pełne utwory, pod warunkiem, że podasz autorstwo oryginalnego wydawcy.<br>
+        Podszywanie się pod inne firmy objęte znakami towarowymi lub chronionymi prawami autorskimi nie jest tolerowane.<br>
+        Jeśli twój kanał jest parodią, musi być to dobrze widoczne.",
+        "ftQA" => "Q&A FulpTube",
+    
+        "ftQASubtext" => "                        <b>Zapomniałem swojego hasła</b><br>
+        Dołącz do naszego Discorda jeżeli chodzi o hasła.<br><br>
+        <b>Czemu?</b><br>
+        Stworzyłem FulpTube jako projekt dla zabawy i nigdy nie spodziewałem się takiego dużego ruchu i uwagi.<br>Będe utrzymywać tę stronę przy życiu jak najdłużej jak mogę.<br>Powinieneś/aś dołączyć do naszego serwera Discord, jeżeli masz ochotę porozmawiać z naszą społecznością.<br><br>
+        <b>Kto jest właścicielem tej strony?</b><br>
+        <a href=\"/user?n=chief%20bazinga\">chief bazinga</a><br>",
+    );
+?>
