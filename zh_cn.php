a240212396d752c90d83defce0eca8a9da34b185
chief bazinga <71347819+the-real-sumsome@users.noreply.github.com>
2021-04-17 21:16:40 -0700

    chinese by wok

---
+++ zh_cn.php
@@ -0,0 +1,551 @@
+<?php
+
+    // [TODO]
+
+    $cLang = array(
+
+        "languageD" => "Chinese (China Mainland)",
+
+
+
+
+        // [categories]
+
+        "recentC" => "FulpTube最新",
+
+        "musicC" => "音乐视频",
+
+        "reccomendedC" => "推荐视频",
+
+        "videoC" => "游戏视频",
+
+        // [video] 
+
+        "byUsr" => "从",
+
+        "agoLeft" => "几个",
+
+        "agoRight" => "", // <agoLeft> <date> <agoRight> ex: 3 秒前
+
+        "justNow" => "刚刚",
+
+        "ago" => "前",
+
+        "country" => "国家",
+
+        "eViews" => "个观看",
+
+        "worldwide" => "国际",
+
+        "gotIt" => "知道!",
+
+        "welcomeToFulpTubeHomepage" => "欢迎来到FulpTube主页",
+
+        // [sidebar]
+
+        "channelsForYou" => "推荐给你的频道",
+
+        "dashboardText" => "你订阅的频道和自我设定的推荐视频会在下面出现. FulpTube根据系统认为你会观看的视频推荐这些视频给你.<br><br>
+
+        看看你订阅的人在做什么<br>
+
+        看看 <a href=\"#\">我的订阅</a>.",
+
+        "subscriptions" => "我的订阅",
+
+        "subscriptionManage" => "管理我的订阅",
+
+        "browseChannels" => "看看其他频道",
+
+        "newestVideos" => "最新视频",
+
+        "signInNow" => "现在登录即可观看你喜欢的频道和更多的推荐视频！",
+
+        // [header]
+
+        "uploadVid" => "上载视频",
+
+        "browse" => "浏览视频",
+
+        "vChannel" => "频道",
+
+        "feed" => "推荐栏",
+
+        "vUploaded" => "已上载的视频",
+
+        "vCancel" => "取消",
+
+        "vDuration" => "影片长度",
+
+        "annoHowtoTitle" => "如何使用:",
+
+        "videoResponseStart" => "<a href=\"/videoresponse?v=",
+
+        "videoResponseEnd" => "\">创立一个新的视频回应</a> 或 ",
+
+        "vViews" => "个观看",
+
+        "vLikes" => "个赞",
+
+        "annoCreator" => "Annotation Maker by Crazy Frog",
+
+        "vDislikes" => "个不赞",
+
+        "toCreateAnnotations" => "若想在视频上加注释, 请上载一个文件.",
+
+        "vStatus" => "状态",
+
+        "toggleCommenting" => "开启/关闭留言",
+
+        "vComments" => "留言",
+
+        "annoHowto" => '设置注释的位子, 大小, 颜色和长度.<br><br>
+
+        按"加进输出"的按钮以保存更改<br><br>
+
+        如果觉得想更改注视, 可以按"复原“按钮
+
+        如果想从新来过, 可以按"清除输出"按钮.<br><br>
+
+        对注释满意过后, 按"复制至剪贴板“按钮吧注释复制进电脑的剪贴板',
+
+        "vURL" => "视频链接",
+
+        "actualUploadVideo" => "上载视频",
+
+        "delete" => "删除",
+
+        "thumbnail" => "缩图",
+
+        "upload" => "上载",
+
+        "signIn" => "登陆",
+
+        // [time]
+
+        "year" => "年",
+
+        "month" => "月",
+
+        "week" => "星期",
+
+        "day" => "日",
+
+        "hour" => "小事",
+
+        "minute" => "分钟",
+
+        "second" => "秒",
+
+
+
+
+        "pluralTimeRight" => "",
+
+        // [watch page]
+
+        "subscribe" => "订阅",
+
+        "language" => "语言",
+
+        "copyright" => "版权",
+
+        "about" => "关于我们",
+
+        "shareVid" => "分享视频",
+
+        "addTo" => "加进...",
+
+        "tags" => "标签",
+
+        "thumbnail" => "缩图",
+
+        "charremaining" => "剩下字数：", // replace * with char count
+
+        "rectag" => "建议的标签",
+
+        "vidStatistics" => "影片数据",
+
+        "showMore" => "更多",
+
+        "prePost" => "unused, do not translate",
+
+        "post" => "发表",
+
+        "cPost" => "发表",
+
+        "showLess" => "收缩",
+
+        "reply" => "回复",
+
+        "shareyourthoughts" => "分享你的意见",
+
+        "allComments" => "所有留言",
+
+        "publishedAt2" => "发布时间",
+
+        "topComments" => "最佳留言",
+
+        "loadMore" => "现实更多",
+
+        // [profile]
+
+        "discussion" => "谈论",
+
+        "recentUploads" => "最近上载",
+
+        "playlists" => "播放清单",
+
+        "profilePic" => "头像",
+
+        "uploadBanner" => "上载横幅",
+
+        "uploadImage" => "上载图片",
+
+        "joined" => "账号创建于",
+
+        "messageProfile" => "私信",
+
+        "uploadedAVideo" => "上载了一个视频",
+
+        "subscribers" => "个订阅",
+
+        "userJoined" => "在 %d 加入",
+
+        "featuredChannel" => "推荐频道",
+
+        "popularChannels" => "热门频道",
+
+        "theseRecommendations" => "这些建议由FulpTube自动创建.",
+
+        // [inbox]
+
+        "newComment" => "新的留言",
+
+        "newSubscriber" => "新的订阅者",
+
+        "inbox" => "信箱",
+
+        "markAll" => "已读所有",
+
+        "compose" => "创建",
+
+        "from" => "从:",
+
+        "sent" => "已发出",
+
+        "sendMessage" => "发出信息",
+
+        "date" => "日期",
+
+        "to" => "至:",
+
+        "subject" => "主题:",
+
+        "messageInbox" => "信息:",
+
+        // [upload]
+
+        "uTitle" => "标题",
+
+        "category" => "类别",
+
+        "privacySetting" => "隐私设置",
+
+        "description" => "简单说明",
+
+        // [sign in]
+
+        "signInBenifits" => "登陆以便可以在FulpTube: ",
+
+        "s1a" => "关注你喜欢的频道",
+
+        "s1b" => "把影片保存到过后才看, 观看为你量身定做的推荐视频, 或者订阅你喜欢的频道.",
+
+        "s2a" => "到哪里都可以观看", 
+
+        "s2b" => "把你喜欢的东西带到哪里都看 ? 在你的手机, 平板电脑或智能电视.",
+
+        "s3a" => "分享视频给朋友",
+
+        "s3b" => "在社交媒体各处看你朋友分享的视频 ? 全在一处.", 
+
+        // [misc]
+
+        //"" => "",
+
+        "username2" => "用户名",
+
+        "password2" => "密码",
+
+        "email2" => "电邮",
+
+        "confirmpw2" => "确认密码",
+
+        "register" => "申请账号",
+
+        "signUp" => "申请账号",
+
+        "orRegister" => "...或者 <a href='/newsignup'>申请账号</a>",
+
+        "unsubscribe" => "取消订阅",
+
+        "addChannels" => "加入更多频道",
+
+        "subscribed" => "已订阅",
+
+        "xVideos" => "视频",
+
+        "uVideos" => "视频",
+
+        "transcribe" => "翻译",
+
+        "statistics" => "数据",
+
+        "report" => "举报",
+
+        "popularChannels" => "FulpTube热门",
+
+        "featuredChannels" => "推荐频道",
+
+        "dateAddedoldNew" => "添加日期 (最旧 - 最新)",
+
+        "dateAddedNewOld" => "添加日期 (最新 - 醉酒)",
+
+        "Uplaylist" => "播放列表",
+
+        "mostPopular" => "最热门",
+
+        "privacy" => "隐私",
+
+        "playlist" => "播放清单",
+
+        "termsAndCond" => "条例",
+
+        "sendFeedback" => "给予回馈",
+
+        "recentUploads" => "最新上传",
+
+        "uploads" => "上载",
+
+        "searchSubscriptions" => "搜索订阅频道",
+
+        "comments" => "留言",
+
+        "inboxComments" => "留言",
+
+        "contactNotifications" => "联系通知",
+
+        "message" => "信息",
+
+        "time" => "时间:",
+
+        "joindate" => "加入日期 ",
+
+        "myChannel" => "我的频道",
+
+        "videoManager" => "影片控制台",
+
+        "fulptubeSettings" => "设置",
+
+        "userSettings" => "用户设置",
+
+        "signOut" => "登出",
+
+        "foogleAccount" => "Foogle账户",
+
+        "thisisyou" => "这是你",
+
+        "overview" => "简介",
+
+        "name" => "名字",
+
+        "password" => "密码",
+
+        "changePW" => "更改密码",
+
+        "cemail" => "你会收到确认更改密码的电邮",
+
+        "misc" => "其他",
+
+        "accCreated" => "账户创建:",
+
+        "loginDate" => "最上一次登陆:",
+
+        "customization" => "客制化",
+
+        "personalMessages" => "个人信息",
+
+        "sharedWithYou" => "分享给你",
+
+        "banner" => "横幅",
+
+        "resWarn" => "注意: 横幅像素建议为821x176. 更高或更低像素的照片将会被自动调整",
+
+        "yourPhoto" => "你的照片",
+
+        "socialLinks" => "社交链接",
+
+        "links" => "链接",
+
+        "customURL" => "自定URL",
+
+        "leaveBlank" => "如果不想设置，请留空白",
+
+        "featureAndDescribe" => "推荐和介绍",
+
+        "describeAndfeature" => "介绍和推荐",
+
+        "sent" => "已发",
+
+        "webVTT" => "注意：只接受WebVTT",
+
+        "edit" => "编辑",
+
+        "maxRes" => "注意：最大大小是1080p",
+
+        "bio" => "个人介绍",
+
+        "captionsNotWorking" => "字母暂时不能使用",
+
+        "anno" => "注释",
+
+        "basicInfo" => "基本资料",
+
+        "videoManager" => "影片控制台",
+
+        "displayName" => "显示名字",
+
+        "addMoreVideos" => "加入更多视频",
+
+        "featuredChannels" => "推荐频道",
+
+        "exampleFeaturedChannels" => "用逗号分开. 不要在逗号后面加空格，而且使用英语逗号. 例如: chief bazinga,帅哥,Joel98",
+
+        "featuredVideo" => "推荐视频",
+
+        "warningFeaturedVideo" => "最后面不可以有空格.例如: MTYwNTQ4ODc4Mja=24 是正确的. 无需加入 FULPTUBE.ROCKS 链接, 只需要影片ID",
+
+        "toggleFlash" => "开启/关闭 Flash 播放器",
+
+        "aboutResults" => "大概 %s 个结果",
+
+        "searchSubscriptions" => "搜索订阅的频道",
+
+        "watchLater" => "过后观看",
+
+        "donotreupload" => "                            <b style=\"font-size: 18px;\">不要上传不属于你的视频</b><br>
+
+        <br>
+
+        缩图是自动创建的. <br>
+
+        所有视频将会使用FFMPEG转换成 720p@1000kbps H264/AAC , 让你使用不同的视频格式.<br>
+
+        视频大小一定要小过 100MB. (因为臭Cloudflare)<br>
+
+        最好不要有其他有版权的东西，不然我们会被抓.<br>
+
+        <b>视频是人手通过的.</b>",
+
+        "watchHistory" => "观看历史",
+
+        "whatToWatch" => "要看什么？",
+
+        "mySubscriptions" => "我的订阅",
+
+        "analytics" => "分析",
+
+        "dashboard" => "控制台",
+
+        "adobeStatus" => "Flash有开吗? ",
+
+        "active" => "开了",
+
+        "guide" => '导航',
+
+        "deactive" => "关了",
+
+
+
+
+        "fulptubeTOS" => "FulpTube规则条例",
+
+        "fulptubeTOSSub" => "                                                <b>基本规则</b><br>
+
+        不要白痴. 这个网站是拿来爽的. <br>
+
+        这是一个致力要模仿2013年的YouTube设计. <br>
+
+        网站的播放器和JS代码属于YouTube的，不是我们的<br>
+
+        FulpTube与谷歌没有任何关系<br>
+
+        所有违反美国法律的内容将会被移除，涉及人事的账号也会被封锁<br>
+
+        所有尝试使别人被封锁的人都会被FulpTube封锁<br>
+
+        不允许成人内容, 偏执, 反同性恋, 然后请用常识. 谢谢.<br><br>
+
+        <b>举报问题 + 举报视频</b><br>
+
+        若发现有视频违反规则, 请在脚本下的discord联系我们.<br>
+
+        发现问题? 在discord的#bug-reports举报.<br><br>
+
+        <b>联系</b><br>
+
+        电邮: spacemygithub@gmail.com<br>
+
+        官方Discord群: In the footer<br>
+
+        Discord: bhief cazinga#5644",
+
+
+
+
+        "ftPrivacy" => "FulpTube 隐私条例 ",
+
+        "ftPrivacySubtext" => "                        <b>隐私</b><br>
+
+        我们不会保存IP号码！ 我们只是保存你的电邮，密码和用户名<br>
+
+        其他保存的东西还有你的视频，留言和其他的东东. 你可以要求索取它们.<br>
+
+        我们不会把你的资料卖出去. <br>
+
+        有隐私挂虑? Discord在上面.<br>",
+
+        
+
+        "ftCopyright" => "FulpTube 版权",
+
+        "ftCopyrightSubtext" => "                                                <b>版权</b><br>
+
+        确定你不是在用别人的音乐来赚自己的钱.<br>
+
+        只要你给予原创者感谢，你都可以把完整的歌曲上载到FulpTube<br>
+
+        不允许冒用他人或其他公司的名义来使用FulpTube<br>
+
+        如果是恶搞，必须明显让别人知道",
+
+        "ftQA" => "FulpTube 问答",
+
+    
+
+        "ftQASubtext" => "                        <b>我忘了密码！</b><br>
+
+        加入Discord解决<br><br>
+
+        <b>为啥?</b><br>
+
+        FulpTube是我做爽的，我完全没料到它会那么红. <br>我会尽量把这个网站保存上线状态.<br>和我们交流可以加入Discord哦<br><br>
+
+        <b>谁是这个网站的主人?</b><br>
+
+        <a href=\"/user?n=chief%20bazinga\">帅哥chief bazinga</a><br>", //帅哥 means cool guy
+
+    );
+
+?>
